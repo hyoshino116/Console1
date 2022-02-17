aabdb085e421088784702f69a232142c79fa880e
Hideo Yoshino <hyoshino@microsoft.com>
2022-02-17 20:11:12 +0900

    change to original

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Console1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello CSAMs!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
